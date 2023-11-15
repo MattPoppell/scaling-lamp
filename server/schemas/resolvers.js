18fa0ea1391b1e99e1dfb861847017ceb1780263
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-11-15 14:53:36 -0500

    Moved over files from original repo.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,128 +1,37 @@
-const { User, Thought } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { Venue, Comment, Profile } = require('../models');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('thoughts');
+    venues: async () => {
+      return Tech.find({});
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
-    },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
-
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
-      }
-      throw AuthenticationError;
+    venue: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Venue.find(params);
     },
   },
-
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-
-      const token = signToken(user);
+    createProfile: async (parent, args) => {
       
-      return { token, user };
     },
-    
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw AuthenticationError;
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw AuthenticationError;
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
+    createComment: async (parent, { venueId, commentText }) => {
+        return Venue.findOneAndUpdate(
+           
+        );
     },
+    updateComment: async (parent, args) => {
 
-    addThought: async (parent, { thoughtText }, context) => {
-      if (context.user) {
+    },
+    removeComment: async (parent, { venueId, commentId }) => {
+        return Venue.findOneAndUpdate(
+          
+        );
+    },
+    createVenue: async (parent, args) => {
         
-        const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw AuthenticationError;
-      ('You need to be logged in!');
     },
+    updateVenue: async (parent, args) => {
 
-    addComment: async (parent, { thoughtId, commentText }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $addToSet: {
-              comments: { commentText, commentAuthor: context.user.username },
-            },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-      }
-      throw AuthenticationError;
-    },
-
-    removeThought: async (parent, { thoughtId }, context) => {
-      if (context.user) {
-
-        const thought = await Thought.findOneAndDelete({
-          _id: thoughtId,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw AuthenticationError;
-    },
-    
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
-              },
-            },
-          },
-          { new: true }
-        );
-      }
-      throw AuthenticationError;
     },
   },
 };
--- server/utils/dateFormat.js
+++ server/utils/dateFormat.js
@@ -51,14 +51,12 @@ module.exports = (
       ? Math.floor(dateObj.getHours() - 12)
       : dateObj.getHours();
 
-  // if hour is 0 (12:00am), change it to 12
   if (hour === 0) {
     hour = 12;
   }
 
   const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
 
-  // set `am` or `pm`
   const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
 
   const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
