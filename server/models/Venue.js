5b97daa65955c096b9eb7b74e42f72fa4bc7ab84
Mathew Poppell <mpoppell1002@gmail.com>
2023-11-15 14:56:18 -0500

    fixed models

---
+++ server/models/Venue.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const venueSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  state: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  capacity: {
+    type: Number,
+    required: false,
+    trim: true,
+  },
+  preferredGenre: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  catering: {
+    type: Boolean,
+    required: false,
+  },
+  barsNearby: {
+    type: Boolean,
+    required: false,
+  }
+
+});
+
+
+
+const Venue = model('Venue', venueSchema);
+
+module.exports = Venue;
