c4333ff15428dfe1fbacc53b86dfc78a643fc912
sunainaojha <sunainaojha1242@gmail.com>
2023-11-20 10:36:00 -0500

    created css file and did styling of components and pages

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth';
+import './Auth.css';
+
+const Login = () => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <main className="auth-container">
+      <div className="auth-card">
+        {data ? (
+          <p>
+            Successfully logged in....{' '}
+            <Link to="/">Back to the homepage.</Link>
+          </p>
+        ) : (
+          <form onSubmit={handleFormSubmit}>
+            <input
+              className="form-input"
+              placeholder="Your email"
+              name="email"
+              type="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+            <input
+              className="form-input"
+              placeholder="******"
+              name="password"
+              type="password"
+              value={formState.password}
+              onChange={handleChange}
+            />
+            <button className="submit-btn" type="submit">
+              Submit
+            </button>
+          </form>
+        )}
+
+        {error && (
+          <div className="error-message">
+            {error.message}
+          </div>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Login;
